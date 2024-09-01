adf5c6f4779cbb52650eef317a3d9734f37dea44
aniket152000 <aniketghare24@gmail.com>
2024-09-01 13:10:00 +0530

    initial commit

---
+++ src/main/java/com/example/mental_health_support_chatbot/MentalHealthSupportChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.example.mental_health_support_chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentalHealthSupportChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentalHealthSupportChatbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mental_health_support_chatbot/MentalHealthSupportChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mental_health_support_chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentalHealthSupportChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
